08fcf967c34f5e900df505b5d526f8ea7483f69d
benjamindenise <benjamin.denise@protonmail.com>
2022-08-09 11:51:23 +0200

    Change JSON to YAML

--- build.py
+++ build.py
@@ -1,9 +1,8 @@
-import json
-
+import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 with open("links.json", "r", encoding="utf8") as links_file:
-    data = json.load(links_file)
+    data = yaml.load(links_file, Loader=yaml.CLoader)
 
 jinja_env = Environment(
     loader=FileSystemLoader("./templates"), autoescape=select_autoescape()
