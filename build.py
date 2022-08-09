8ed104a2e5b52f96e23f0e68a42b0491d141cbda
benjamindenise <benjamin.denise@protonmail.com>
2022-08-09 12:05:38 +0200

    Add source directory

--- build.py
+++ build.py
@@ -1,14 +1,24 @@
+import shutil
+import os
+
 import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-with open("links.json", "r", encoding="utf8") as links_file:
+os.mkdir("./dist")
+
+# Build the index file with Jinja2
+with open("links.yaml", "r", encoding="utf8") as links_file:
     data = yaml.load(links_file, Loader=yaml.CLoader)
 
 jinja_env = Environment(
-    loader=FileSystemLoader("./templates"), autoescape=select_autoescape()
+    loader=FileSystemLoader("./src"), autoescape=select_autoescape()
 )
 index_template = jinja_env.get_template("index.html")
 index_render = index_template.render(links=data)
 
-with open("dist/index.html", "w", encoding="utf8") as file:
+# Save the new index file
+with open("./dist/index.html", "w", encoding="utf8") as file:
     file.write(index_render)
+
+# Copy the assets directory
+shutil.copytree("./src/assets", "./dist/assets")
