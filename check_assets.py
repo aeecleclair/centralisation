e6ae05e6661318a3ff1c3dae77a7a33d45af7c71
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-08-15 11:22:27 +0200

    Workflow and improvements (#6)
    * Use img label in yaml for svg files
    
    * Add a test workflow
    
    * Test workflow: install Poetry before Python
    
    * Test workflow: upload artifacts
    
    * Test workflow: comment CI result
    
    * Cache publish workflow
    
    * Address review
    
    * Add BDS img field
    
    * Remove unneeded quotes in yaml file

---
+++ check_assets.py
@@ -0,0 +1,14 @@
+from os.path import exists
+
+import yaml
+
+# Build the index file with Jinja2
+with open("links.yaml", "r", encoding="utf8") as links_file:
+    data = yaml.load(links_file, Loader=yaml.CLoader)
+
+for category_name in data:
+    for link in data[category_name]:
+        if not exists("src/assets/icons/" + link["img"]):
+            raise ValueError(
+                f"File {link['img']} is missing from the assets directory"
+            )
