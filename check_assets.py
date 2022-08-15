b39cb7b1da00b579ac4c7fe686054a781a8abbbc
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-08-15 08:16:26 +0200

    Address review

--- check_assets.py
+++ check_assets.py
@@ -8,8 +8,7 @@ with open("links.yaml", "r", encoding="utf8") as links_file:
 
 for category_name in data:
     for link in data[category_name]:
-        for key in ["name", "description", "img", "url"]:
-            if key not in link:
-                raise KeyError(f"Missing key {key} for {link['name']}")
         if not exists("src/assets/icons/" + link["img"]):
-            raise ValueError(f"Inexistant asset {link['img']} for {link['name']}")
+            raise ValueError(
+                f"File {link['img']} is missing from the assets directory"
+            )
