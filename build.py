43575139d7c46b9765f83d7e21bb2e7ba15f706c
Daihecyy <paul.saubusse@gmail.com>
2024-03-15 22:51:14 +0100

    Début flipping div

--- build.py
+++ build.py
@@ -10,18 +10,21 @@ if os.path.exists("./dist"):
 os.mkdir("./dist")
 
 # Build the index file with Jinja2
-with open("links.yaml", "r", encoding="utf8") as links_file:
+with open("links2.yaml", "r", encoding="utf8") as links_file:
     data = yaml.load(links_file, Loader=yaml.CLoader)
 
 jinja_env = Environment(
     loader=FileSystemLoader("./src"), autoescape=select_autoescape()
 )
-index_template = jinja_env.get_template("index.html")
-index_render = index_template.render(links=data)
+other_pages = [{"link": page["link"], "name": page["name"]} for page in data]
+for page in data:
+    file_path = page["link"] + ".html"
+    page_template = jinja_env.get_template("index2.html")
+    page_render = page_template.render(links=page["children"], other_pages=other_pages)
 
-# Save the new index file
-with open("./dist/index.html", "w", encoding="utf8") as file:
-    file.write(index_render)
+    # Save the new index file
+    with open("./dist/" + file_path, "w", encoding="utf8") as file:
+        file.write(page_render)
 
 # Copy the assets directory
 shutil.copytree("./src/assets", "./dist/assets")
